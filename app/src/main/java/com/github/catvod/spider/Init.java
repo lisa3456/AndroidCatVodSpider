91c7ac4e9e65275b11fd74706545e765e097e73c
youyou <93966747+lisa3456@users.noreply.github.com>
2026-07-30 12:57:15 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Init.java
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -8,11 +8,12 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+
 import com.github.catvod.crawler.SpiderDebug;
-import com.github.catvod.utils.Launcher;
-// import com.github.catvod.utils.ProxyServer;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -34,6 +35,19 @@ public class Init {
     public Init() {
         this.handler = new Handler(Looper.getMainLooper());
         this.executor = Executors.newFixedThreadPool(5);
+        // Thread serverThread = new Thread(() -> {
+        //     try {
+        //         XiaoyaProxyServer.get().start();
+        //     } catch (Exception e) {
+        //         return;
+        //     }
+        // });
+
+        // serverThread.setUncaughtExceptionHandler((Thread thread, Throwable throwable) -> {
+        //     //Logger.log("未捕获异常：" + throwable.getMessage(), true);
+        // });
+        
+        // serverThread.start();
     }
 
     public static Application context() {
@@ -43,10 +57,6 @@ public class Init {
     public static void init(Context context) {
         get().app = ((Application) context);
         SpiderDebug.log("自定義爬蟲代碼載入成功！");
-        execute(() -> {
-          //  ProxyServer.INSTANCE.start();
-            Launcher.startServer(context);
-        });
     }
 
     public static void execute(Runnable runnable) {
@@ -64,15 +74,36 @@ public class Init {
     public static void checkPermission() {
         try {
             Activity activity = Init.getActivity();
-            if (activity == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return;
-            if (activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            if (activity == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
                 return;
-            activity.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 9999);
+            }
+    
+            // 检查是否已经授予了读权限和写权限
+            boolean hasReadPermission = activity.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+            boolean hasWritePermission = activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    
+            // 如果已经授予了读权限和写权限，直接返回
+            if (hasReadPermission && hasWritePermission) {
+                return;
+            }
+    
+            // 申请读权限和写权限
+            List<String> permissionsToRequest = new ArrayList<>();
+            if (!hasReadPermission) {
+                permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+            if (!hasWritePermission) {
+                permissionsToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+    
+            if (!permissionsToRequest.isEmpty()) {
+                activity.requestPermissions(permissionsToRequest.toArray(new String [permissionsToRequest.size()]), 9999);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    
     public static Activity getActivity() throws Exception {
         Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
         Object activityThread = activityThreadClass.getMethod("currentActivityThread").invoke(null);
